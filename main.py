490b72481d4aa55f674b0af00c62fd1ec3e1cce8
Fozila0224 <68692215+Fozila0224@users.noreply.github.com>
2025-03-20 17:48:24 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import plotly.express as px
 import platform
 plt = platform.system()
 if plt == 'Linux': pathlib.WindowsPath = pathlib.PosixPath
-# temp = pathlib.PosixPath
-# pathlib.PosixPath = pathlib.WindowsPath
 
 st.title("Transportni klassifikatsiya qiluvchi model")
 
